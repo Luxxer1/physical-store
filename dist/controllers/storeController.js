e572a927995052be569d32c63388d4e3444f212e
Lucas Bastos <lfsouzabastos@gmail.com>
2025-02-27 18:09:13 -0300

    checks if req.param cep is valid

--- dist/controllers/storeController.js
+++ dist/controllers/storeController.js
@@ -5,14 +5,53 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getNearbyStores = exports.getAllStores = void 0;
 const storeModel_1 = __importDefault(require("../models/storeModel"));
+const node_fetch_1 = __importDefault(require("node-fetch"));
 const getAllStores = async (req, res) => {
     const stores = await storeModel_1.default.find();
-    console.log(stores);
     res.status(200).json({
         status: 'success',
+        data: {
+            stores,
+        },
     });
 };
 exports.getAllStores = getAllStores;
-const getNearbyStores = async (req, res) => { };
+const getNearbyStores = async (req, res) => {
+    let { cep } = req.params;
+    if (!/^\d{8}$/.test(cep)) {
+        res.status(400).json({
+            status: 'error',
+            message: 'CEP inválido. O CEP deve ter exatamente 8 dígitos numéricos.',
+        });
+        return;
+    }
+    try {
+        const cepResponse = await (0, node_fetch_1.default)(`https://viacep.com.br/ws/${cep}/json/`);
+        if (!cepResponse.ok) {
+            res.status(404).json({
+                status: 'error',
+                message: 'Erro ao acessar a API do ViaCEP',
+            });
+            return;
+        }
+        const cepData = (await cepResponse.json());
+        if (cepData.erro) {
+            res.status(404).json({
+                status: 'error',
+                message: 'CEP não encontrado',
+            });
+            return;
+        }
+        console.log(cepData);
+        res.status(200).json({
+            status: 'success',
+        });
+    }
+    catch (err) {
+        res.status(500).json({
+            status: 'error',
+        });
+    }
+};
 exports.getNearbyStores = getNearbyStores;
 //# sourceMappingURL=storeController.js.map
--- src/controllers/storeController.ts
+++ src/controllers/storeController.ts
@@ -1,22 +1,79 @@
 import Store from '../models/storeModel';
 import { Request, Response } from 'express';
-// import fetch from 'node-fetch';
+import fetch from 'node-fetch';
+
+interface CepResponse {
+  cep: string;
+  logradouro: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  ibge: string;
+  gia: string;
+  ddd: string;
+  siafi: string;
+  erro?: boolean;
+}
 
 export const getAllStores = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   const stores = await Store.find();
-  // const response = await fetch('https://viacep.com.br/ws/50930070/json/');
-  // const data = await response.json();
-
-  console.log(stores);
   res.status(200).json({
     status: 'success',
+    data: {
+      stores,
+    },
   });
 };
 
 export const getNearbyStores = async (
   req: Request,
   res: Response
-): Promise<void> => {};
+): Promise<void> => {
+  let { cep } = req.params;
+
+  if (!/^\d{8}$/.test(cep)) {
+    res.status(400).json({
+      status: 'error',
+      message: 'CEP inválido. O CEP deve ter exatamente 8 dígitos numéricos.',
+    });
+    return;
+  }
+
+  try {
+    const cepResponse = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+
+    if (!cepResponse.ok) {
+      res.status(404).json({
+        status: 'error',
+        message: 'Erro ao acessar a API do ViaCEP',
+      });
+      return;
+    }
+
+    const cepData = (await cepResponse.json()) as CepResponse;
+
+    if (cepData.erro) {
+      res.status(404).json({
+        status: 'error',
+        message: 'CEP não encontrado',
+      });
+      return;
+    }
+
+    // Calcular distancia
+
+    console.log(cepData);
+
+    res.status(200).json({
+      status: 'success',
+    });
+    // const data = await response.json();
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+    });
+  }
+};
