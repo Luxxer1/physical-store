f0659c43e095cd957ff949696f56f5c1b1e50f7d
Lucas Bastos <lfsouzabastos@gmail.com>
2025-02-27 18:54:54 -0300

    remove comments

--- src/controllers/errorController.ts
+++ src/controllers/errorController.ts
@@ -1,14 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import AppError from '../utils/appError';
 
-// Middleware de erro
 export const globalErrorHandler = (
   err: AppError,
   req: Request,
   res: Response,
   next: NextFunction
 ): void => {
-  // Se o erro não for uma instância de AppError, criamos um erro genérico
   if (!(err instanceof AppError)) {
     err = new AppError('Algo deu errado!', 500);
   }
