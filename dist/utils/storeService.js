65df48bb6b72ca3589c87f342cb9d1fb1f8a9e79
Lucas Bastos <lfsouzabastos@gmail.com>
2025-03-07 17:25:53 -0300

    generated js files

--- dist/utils/storeService.js
+++ dist/utils/storeService.js
@@ -8,13 +8,14 @@ const axios_1 = __importDefault(require("axios"));
 const storeModel_1 = __importDefault(require("../models/storeModel"));
 const appError_1 = __importDefault(require("../utils/appError"));
 const MAX_DISTANCE_KM = 100;
+const METERS_IN_KM = 1000;
 const calculateDistance = async (origin, destination) => {
     const { data } = await axios_1.default.get(`https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${process.env.API_KEY}`);
     if (data.status !== 'OK' || !data.routes || !data.routes.length) {
         throw new appError_1.default('Não foi possível calcular a distância.', 500);
     }
     const distance = data.routes[0].legs[0].distance.value;
-    return distance / 1000;
+    return distance / METERS_IN_KM;
 };
 exports.calculateDistance = calculateDistance;
 const getNearbyStoresWithDistance = async (origin) => {
