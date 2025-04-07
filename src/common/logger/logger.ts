8fd43f198bb85c3ff1dd116336cb4f10256b56d6
Lucas Bastos <lfsouzabastos@gmail.com>
2025-04-07 20:17:13 -0300

    added logger and middlware logger

---
+++ src/common/logger/logger.ts
@@ -0,0 +1,26 @@
+import winston from 'winston';
+
+const { combine, timestamp, printf, colorize, json } = winston.format;
+
+const consoleFormat = combine(
+  colorize(),
+  timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
+  printf(({ timestamp, level, message }) => {
+    return `${timestamp as string} [${level}]: ${message as string}`;
+  }),
+);
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: combine(timestamp(), json()),
+  transports: [
+    new winston.transports.Console({
+      format: consoleFormat,
+    }),
+
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
+export default logger;
