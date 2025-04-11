c41a1b5b6e9e8f895d619dd9ed762d6a9a704e9f
Lucas Bastos <lfsouzabastos@gmail.com>
2025-04-11 19:23:53 -0300

    i mongodb mongoose

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from 'src/common/logger/logger.middleware';
+import { StoreController } from 'src/store/store.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, StoreController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
