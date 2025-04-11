7f73a11252baa3f02d61221c2e90fbdba9217ee4
Lucas Bastos <lfsouzabastos@gmail.com>
2025-04-11 20:15:18 -0300

    .env configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,19 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from 'src/common/logger/logger.middleware';
-import { StoreController } from 'src/store/store.controller';
 import { ConfigModule } from '@nestjs/config';
+import { StoreModule } from 'src/store/store.module';
+import configuration from 'src/config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [configuration],
     }),
+    StoreModule,
   ],
-  controllers: [AppController, StoreController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
