3ac5114033fb2922568dc2c112c6c24ad7f275ff
Lucas Bastos <lfsouzabastos@gmail.com>
2025-04-14 17:06:29 -0300

    npm axios

--- src/store/store.service.ts
+++ src/store/store.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Store, StoreDocument } from './store.model';
 import logger from 'src/common/logger/logger';
 
 @Injectable()
 export class StoreService {
   private readonly MAX_DISTANCE_KM = 100;
   private readonly METERS_IN_KM = 1000;
+
+  constructor(
+    @InjectModel(Store.name) private readonly storeModel: Model<StoreDocument>,
+    private readonly httpService: HttpService,
+  ) {}
 
   async getAllStores() {
     logger.info('Buscando todas as lojas...');
